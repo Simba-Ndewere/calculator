66b81abbeb4124afeac3b1a894870713b2f6c47d
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-10 22:41:20 +0000

    add button clicked to divs

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,6 +1,8 @@
 let currentNumber = 0;
 let secondNumber = 0;
 let operator = '';
+let bottom = '';
+let top = '';
 
 function operate(operator, currentNumber, secondNumber){
     switch(operator){
@@ -12,21 +14,32 @@ function operate(operator, currentNumber, secondNumber){
         break;
         case '/': divide(currentNumber,secondNumber);
         break;
+        case '%': percent(currentNumber,secondNumber);
+        break;
     }
 }
 
 function add(num1, num2){
+    operator = '';
     return num1+num2;
 }
 
 function subtract(num1,num2){
+    operator = '';
     return num1-num2;
 }
 
 function multiply(num1, num2){
+    operator = '';
     return num1*num2;
 }
 
 function divide(num1, num2){
+    operator = '';
     return num1/num2;
+}
+
+function percent(num1, num2){
+    operator = '';
+    return num1%num2;
 }
