564030984442a4e2b365d7fad5d1cda5279b4af1
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-11 03:05:04 +0000

    add values to text fields

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -5,92 +5,95 @@ let bottomShow = '';
 let topShow = '';
 let result = 0;
 
-function deleteClicked(){
+function deleteClicked() {
     let value = arguments[0];
-    console.log(value);
 }
 
-function orangeClicked(){
+function orangeClicked() {
     let value = arguments[0];
-    if(value=='-' && firstNumber == ''){
+
+    if (value == '-' && firstNumber == '') {
         firstNumber = firstNumber + '-';
     }
 
-    if(value=='+'){
+    if (value == '+') {
         operator = '+';
     }
 
-    if(value=='-'){
-        operator = '-';
+    if (value == '-') {
+        operator = '-';ct
     }
 
-    if(value=='*'){
+    if (value == '*') {
         operator = '*';
     }
 
-    if(value=='/'){
+    if (value == '/') {
         operator = '/';
     }
 
-    if(value=='='){
-        console.log(firstNumber);
-        console.log('space');
-        console.log(secondNumber);
-        console.log('space');
-        console.log(operator);
-        console.log('space');
-        console.log(operate(operator,+firstNumber,+secondNumber));
+    if (value == '=') {
+        result = operate(operator, +firstNumber, +secondNumber);
+        clearValues();
     }
-
 }
 
-function numbersClicked(){
+function numbersClicked() {
     let value = arguments[0];
-    console.log(operator + 'operator');
-    if(operator==''){
+    display(value);
+    if (operator == '') {
         firstNumber = firstNumber + value;
-        console.log(firstNumber + 'first');
-    }else{
+        operator = '';
+    } else {
         secondNumber = secondNumber + value;
     }
 }
 
-function operate(operator, firstNumber, secondNumber){
-    switch(operator){
-        case '+': return add(firstNumber,secondNumber);
-        
-        case '-': return subtract(firstNumber,secondNumber);
-        
-        case '*': return multiply(firstNumber,secondNumber);
-        
-        case '/': return divide(firstNumber,secondNumber);
-       
-        case '%': return percent(firstNumber,secondNumber);
-       
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case '+': return add(firstNumber, secondNumber);
+        case '-': return subtract(firstNumber, secondNumber);
+        case '*': return multiply(firstNumber, secondNumber);
+        case '/': return divide(firstNumber, secondNumber);
+        case '%': return percent(firstNumber, secondNumber);
     }
 }
 
-function add(num1, num2){
+function add(num1, num2) {
     operator = '';
-    return num1+num2;
+    return num1 + num2;
 }
 
-function subtract(num1,num2){
+function subtract(num1, num2) {
     operator = '';
-    return num1-num2;
+    return num1 - num2;
 }
 
-function multiply(num1, num2){
+function multiply(num1, num2) {
     operator = '';
-    return num1*num2;
+    return num1 * num2;
 }
 
-function divide(num1, num2){
+function divide(num1, num2) {
     operator = '';
-    return num1/num2;
+    return num1 / num2;
 }
 
-function percent(num1, num2){
+function percent(num1, num2) {
     operator = '';
-    return num1%num2;
+    return num1 % num2;
+}
+
+function clearValues() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+}
+
+function display(value){
+    console.log(value);
+    let bottom = document.querySelector(".bottom");
+    let top = document.querySelector(".top");
+    bottom.textContent = value;
+    top.textContent = firstNumber + operator + secondNumber;
 }
