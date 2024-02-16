9961c6b03b634e5a51a7d2817306d851c061b8f8
Simba Ndewere <ndeweresimba@gmail.com>
2024-02-16 19:11:25 +0000

    commit

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -206,7 +206,8 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    let answer = num2 * num1;
+    return answer.toFixed(4);
 }
 
 function divide(num1, num2) {
